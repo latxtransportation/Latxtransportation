8d63ceb66eee1ae395148a3d465c2b7a212f0910
TinsaeMia <tinsaetesfaye2003@gmail.com>
2025-10-18 19:56:24 +0300

    changesagain9

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ import Programs from "@/components/programs";
 
 export default function Home() {
   return <main className="min-h-screen">
- 
+    <head>
+      <title>Home  </title>
+    </head>
+
     <Hero />
     <Programs />  
     </main>;
