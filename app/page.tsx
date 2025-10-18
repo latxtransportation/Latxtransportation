7faad8e5252eac692929abc95c87260114b632ab
TinsaeMia <tinsaetesfaye2003@gmail.com>
2025-10-18 20:04:06 +0300

    changesagain9

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,29 @@
 
 import Hero from "@/components/hero";
 import Programs from "@/components/programs";
+import Head from "next/head";
 
 
 
 export default function Home() {
   return <main className="min-h-screen">
-    <head>
-      <title>Home  </title>
-    </head>
+ <Head>
+  <title>Latxtransportation | Safe & Reliable Rides</title>
+  <meta
+    name="description"
+    content="LATX Transportation — providing safe, compassionate non-emergency medical transport in Central Texas."
+  />
+  <meta property="og:title" content="Latxtransportation | Safe & Reliable Rides" />
+  <meta
+    property="og:description"
+    content="LATX Transportation — providing safe, compassionate non-emergency medical transport in Central Texas."
+  />
+  <meta property="og:url" content="https://www.latxtransportation.com" />
+  <meta property="og:site_name" content="Latxtransportation" />
+ 
+  <meta name="twitter:card" content="summary_large_image" />
+</Head>
+
 
     <Hero />
     <Programs />  
